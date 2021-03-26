f4a0d7c50b8775dfdb40de76a236f9c5988935f9
Robert Dolgopolov <siidousdart@gmail.com>
2021-03-26 19:03:41 +0500

    rainbow

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+        public static void main(String[] args) {
+            String text = "Каждый охотник желает знать, где сидит фазан";
+            String[] colors = text.split(",?\\s+");
+            String temp;
+            for (int i = 0; i < colors.length / 2; i++) {
+                temp = colors[colors.length - i - 1];
+                colors[colors.length - i - 1] = colors[i];
+                colors[i] = temp;
+            }
+            for (String sout : colors) {
+                System.out.println(sout);
+            }
+        }
+    }
+
